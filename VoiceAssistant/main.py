9c220138156915d11b092561ae1adc7dd0049aac
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-01-04 17:33:02 -0600

    Create main.py

---
+++ VoiceAssistant/main.py
@@ -0,0 +1,28 @@
+import speech_recognition as speechRec
+from audioplayer import AudioPlayer
+
+listener = speechRec.Recognizer()
+
+try:
+    with speechRec.Microphone() as source:
+        print("I'm waiting for your instructions")
+        AudioPlayer("audio/Hello.wav").play(block=True)
+
+
+        voice = listener.listen(source)
+        userInput = listener.recognize_google(voice)
+        print(userInput)
+
+
+
+        if 'hello' in userInput:
+            print('it worked!')
+
+
+
+
+
+except:
+    pass
+
+
