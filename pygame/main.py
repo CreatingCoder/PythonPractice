c5148c3306d53339956fb83a5e32eb0d7c1ada03
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-07-31 19:38:58 -0400

    Create main.py

---
+++ pygame/main.py
@@ -0,0 +1,24 @@
+import pygame
+import sys
+
+pygame.init()
+scr = pygame.display.set_mode((350, 600))
+clock = pygame.time.Clock()
+
+run = True
+
+def draw():
+    scr.fill('lightblue')
+
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame,quit()
+            sys.exit
+
+
+    draw()
+
+    clock.tick(60)
+    pygame.display.update()
