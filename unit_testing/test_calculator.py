85028fae827a5176c1af51391872d6916587a873
Shane <69217993+CreatingCoder@users.noreply.github.com>
2025-01-19 13:33:33 -0800

    Update test_calculator.py

--- unit_testing/test_calculator.py
+++ unit_testing/test_calculator.py
@@ -1,5 +1,11 @@
 import unittest
-from calculator import Calculator
+import calculator
 
-calc = Calculator()
+class TestCalculator(unittest.TestCase):
 
+    def test_add(self):
+        self.assertEqual(calculator.add(1,6), 7)
+
+
+if __name__ == '__main__':
+    unittest.main()
